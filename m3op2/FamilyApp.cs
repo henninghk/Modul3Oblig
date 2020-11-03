8794f914bd09a40418f2416bb2acd2483974058c
henninghk <64491564+henninghk@users.noreply.github.com>
2020-11-03 14:28:55 +0100

    311

---
+++ m3op2/FamilyApp.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace m3op2
+{
+    public class FamilyApp
+    {
+        public List<Person> _people;
+        public FamilyApp(params Person[] people)
+        {
+            _people = new List<Person>(people);
+        }
+        internal string WelcomeMessage = "Hei! Welkommen til Family Appen!";
+        internal string CommandPrompt = "Commandoene er hjelp, liste og vis \n";
+
+        public object HandleCommand(string command)
+        {
+            int childrenSearchId = 0;
+            string str = "";
+            if (command == "hjelp")
+            {
+                str += @"hjelp => viser en hjelpetekst som forklarer alle kommandoene 
+                         liste => lister alle personer med id, fornavn, fødselsår, dødsår og navn og id på mor og far om det finnes registrert. 
+                         vis <id> => viser en bestemt person med mor, far og barn (og id for disse, slik at man lett kan vise en av dem)";
+            }
+
+            if (command == "liste")
+            {
+
+                foreach (var person in _people)
+                {
+                    str += person.GetDescription() + "\n";
+                }
+            }
+
+            if (command.Substring(0, 3) == "vis" && command.Length > 4)
+            {
+                int searchId = int.Parse(command.Substring(command.Length - 2));
+                foreach (var person in _people)
+                {
+                    if (person.Id == searchId)
+                    {
+                        str += person.GetDescription();
+                        childrenSearchId = person.Id;
+                    }
+                }
+            }
+            str = FindChildren(childrenSearchId, str);
+            
+
+            return str;
+        }
+
+        private string FindChildren(int childrenSearchId, string str)
+        {
+            if (childrenSearchId != 0)
+            {
+                int count = 0;
+                foreach (var person in _people)
+                {
+                    
+                        if (person.Father != null && childrenSearchId == person.Father.Id)
+                        {
+                            if (count == 0)
+                            {
+                                str += "\n  Barn:";
+                                count++;
+                            }
+                            str += $"\n    {person.FirstName} (Id={person.Id}) Født: {person.BirthYear}";
+                        }
+                        if (person.Mother != null && childrenSearchId == person.Mother.Id)
+                        {
+                            if (count == 0)
+                            {
+                                str += "\n  Barn:";
+                                count++;
+                            }
+                            str += $"\n    {person.FirstName} (Id={person.Id}) Født: {person.BirthYear}";
+                        }
+                    
+                }
+
+                if (count > 0)
+                {
+                    str += "\n";
+                }
+            }
+
+            return str;
+        }
+
+        
+        
+    }
+}
---
+++ m3op2/Person.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace m3op2
+{
+    public class Person
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int BirthYear { get; set; }
+        public int DeathYear { get; set; }
+        public Person Father { get; set; }
+        public Person Mother { get; set; }
+
+        public string GetDescription()
+        {
+            if (FirstName == null || LastName == null || BirthYear == 0 || DeathYear == 0 || Father == null || Mother == null)
+            {
+                return CheckWhichEmptyField();
+            }
+
+            return $"{FirstName} {LastName} (Id={Id}) Født: {BirthYear} Død: {DeathYear} Far: {Father.FirstName} (Id={Father.Id}) Mor: {Mother.FirstName} (Id={Mother.Id})";
+        }
+
+        private string CheckWhichEmptyField()
+        {
+
+            string str = "";
+            if (FirstName != null)
+            {
+                str += $"{FirstName} ";
+            }
+            if (LastName != null)
+            {
+                str += $"{LastName} ";
+            }
+            if (Id != 0)
+            {
+                str += $"(Id={Id}) ";
+            }
+            if (BirthYear != 0)
+            {
+                str += $"Født: {BirthYear} ";
+            }
+            if (DeathYear != 0)
+            {
+                str += $"Død: {DeathYear} ";
+            }
+            if (Father != null)
+            {
+                str += $"Far: {Father.FirstName} (Id={Father.Id}) ";
+            }
+            if (Mother != null)
+            {
+                str += $"Mor: {Mother.FirstName} (Id={Mother.Id})";
+            }
+
+            return str;
+        }
+    }
+}
